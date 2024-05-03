5a8fc946452eb48cc0fcce069833c17dd131c38a
Subham Mani <manisubham09@gmail.com>
2024-05-03 14:49:01 +0530

    Refactor routing in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui/button";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Auth from "./components/Auth";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Auth />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <>
-      <div>Hello World!</div>
-      <Button>Click me</Button>
-    </>
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   );
 }
 
