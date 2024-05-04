fa91f0ff63ddca783cb7fc1d04644b3a76ad32dc
Subham Mani <manisubham09@gmail.com>
2024-05-04 23:36:03 +0530

    Add defaultProfilePhoto constant

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+const defaultProfilePhoto =
+  "https://static.vecteezy.com/system/resources/thumbnails/020/911/740/small_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png";
+
+export { defaultProfilePhoto };
