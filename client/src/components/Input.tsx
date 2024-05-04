5e7d799d558904b3697f33a6977aad113025e426
Subham Mani <manisubham09@gmail.com>
2024-05-04 23:36:18 +0530

    Add Input component

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search..."
+      className="w-full bg-transparent focus-within:outline-none px-4"
+    />
+  );
+};
+
+export default Input;
