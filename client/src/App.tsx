3ca9fa8edff2928f4bf3d96eb3de9e1560a98a90
Subham Mani <manisubham09@gmail.com>
2024-05-04 23:35:40 +0530

    Add Redux store to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Auth from "./components/Auth";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import { Provider } from "react-redux";
+import appStore from "./redux/appStore";
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -23,7 +25,9 @@ function App() {
 
   return (
     <div>
-      <RouterProvider router={appRouter} />
+      <Provider store={appStore}>
+        <RouterProvider router={appRouter} />
+      </Provider>
     </div>
   );
 }
