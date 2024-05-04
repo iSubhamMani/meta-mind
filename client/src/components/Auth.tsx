7ffa77dfdba78262ac463ad342c9dacffe024398
Subham Mani <manisubham09@gmail.com>
2024-05-04 23:36:24 +0530

    Add user to state and remove user from state based on authentication status

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,18 +1,23 @@
+import { addUserToState, removeUserFromState } from "@/redux/userSlice";
 import { auth } from "@/utils/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const Auth = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
+        dispatch(addUserToState(user));
         setLoading(false);
         navigate("/home");
       } else {
+        dispatch(removeUserFromState());
         setLoading(false);
         navigate("/login");
       }
