637d94d6bfdddceba5ad57ebc520084fc7df9114
Subham Mani <manisubham09@gmail.com>
2024-05-11 15:00:59 +0530

    Update default profile photo URL and add form schema

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
 const defaultProfilePhoto =
-  "https://static.vecteezy.com/system/resources/thumbnails/020/911/740/small_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png";
+  "https://t3.ftcdn.net/jpg/02/10/49/86/360_F_210498655_ywivjjUe6cgyt52n4BxktRgDCfFg8lKx.jpg";
 
 export { defaultProfilePhoto };
---
+++ client/src/utils/formSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is too short" })
+    .max(200, { message: "Title is too long" })
+    .trim(),
+  body: z.string().min(5, { message: "Body is too short" }).trim(),
+});
+
+export default formSchema;
