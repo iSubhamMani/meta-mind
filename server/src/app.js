f3bb3868ccf9184711d6ffe67ac4d4ef1eacc4bf
Subham Mani <manisubham09@gmail.com>
2024-05-13 00:40:17 +0530

    Add CORS middleware and update dependencies

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,31 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(
+  express.json({
+    limit: "32kb",
+  })
+);
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// routes
+import errorHandler from "./utils/errorHandler.js";
+import postRouter from "./routes/post.route.js";
+import userRouter from "./routes/user.route.js";
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
+
+app.use(errorHandler);
 
 export { app };
