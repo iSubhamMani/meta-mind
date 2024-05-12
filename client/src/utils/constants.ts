462fbbd92b6889dd26585e9931eddbca11e82497
Subham Mani <manisubham09@gmail.com>
2024-05-13 00:40:03 +0530

    Update constants.ts with new profile photo and server URL

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,6 @@
-const defaultProfilePhoto =
+const DEFAULT_PROFILE_PHOTO =
   "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg";
 
-export { defaultProfilePhoto };
+const SERVER_URL = "http://localhost:8000";
+
+export { DEFAULT_PROFILE_PHOTO, SERVER_URL };
