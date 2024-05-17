d64f509b29d89dfae6e0eff0c8264b991fee739c
Subham Mani <manisubham09@gmail.com>
2024-05-17 21:58:11 +0530

    Add new post functionality and additional post endpoints

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -4,19 +4,20 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
 const addNewPost = asyncHandler(async (req, res) => {
-  const { title, body, user } = req.body;
+  const { title, description, body, user } = req.body;
 
   if (!user) {
     throw new ApiError(401, "Unauthorized user");
   }
 
-  if (!title || !body) {
-    throw new ApiError(400, "Title and body are required");
+  if (!title || !body || !description) {
+    throw new ApiError(400, "Title, description and body are required");
   }
 
   const post = await Post.create({
     author: user?.uid,
     title,
+    description,
     body,
   });
 
@@ -29,4 +30,63 @@ const addNewPost = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, "Post created successfully", post));
 });
 
-export { addNewPost };
+const getFeaturedPosts = asyncHandler(async (req, res) => {
+  // get the featured posts
+  const { user } = req.body;
+  const { page = 1, limit = 5 } = req.query;
+
+  if (!user) throw new ApiError(401, "Unauthorized user");
+
+  const options = {
+    page,
+    limit,
+  };
+
+  const pipeline = [
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "author",
+        foreignField: "_id",
+        as: "author",
+        pipeline: [
+          {
+            $project: {
+              __v: 0,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        author: {
+          $first: "$author",
+        },
+      },
+    },
+  ];
+
+  const posts = await Post.aggregatePaginate(Post.aggregate(pipeline, options));
+
+  return res.status(200).json(new ApiResponse(200, "Featured posts", posts));
+});
+
+const getPostById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) throw new ApiError(400, "Post id is required");
+
+  const post = await Post.findById(id, { __v: 0 }).populate("author", "-__v");
+
+  if (!post) throw new ApiError(404, "Post not found");
+
+  return res.status(200).json(new ApiResponse(200, "Post found", post));
+});
+
+export { addNewPost, getFeaturedPosts, getPostById };
--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { addNewPost } from "../controllers/post.controller.js";
+import {
+  addNewPost,
+  getFeaturedPosts,
+  getPostById,
+} from "../controllers/post.controller.js";
 
 const postRouter = Router();
 
 postRouter.route("/add-post").post(addNewPost);
+postRouter.route("/featured").post(getFeaturedPosts);
+postRouter.route("/get-post/:id").get(getPostById);
 
 export default postRouter;
