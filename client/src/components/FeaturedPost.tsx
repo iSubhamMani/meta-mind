db15cd897ebdef713873905cb6108c55ae05feeb
Subham Mani <manisubham09@gmail.com>
2024-05-17 21:59:13 +0530

    Add FeaturedPost component

---
+++ client/src/components/FeaturedPost.tsx
@@ -0,0 +1,43 @@
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { DEFAULT_PROFILE_PHOTO } from "@/utils/constants";
+import { Bookmark } from "lucide-react";
+import { Separator } from "./ui/separator";
+import Post from "@/interfaces/Post";
+import { Link } from "react-router-dom";
+
+const FeaturedPost = ({ post }: { post: Post }) => {
+  return (
+    <div className="pb-5 md:pb-6">
+      <div className="flex items-center gap-3">
+        <Avatar className="w-6 h-6 sm:w-8 sm:h-8 dark:border-2 border-gray-300">
+          <AvatarImage
+            src={post?.author.photoURL || DEFAULT_PROFILE_PHOTO}
+            alt="photo"
+          />
+        </Avatar>
+        <span className="text-black dark:text-white text-sm font-bold line-clamp-1">
+          {post?.author.displayName}
+        </span>
+      </div>
+      <Link to={`/post/${post?._id}`}>
+        <div className="mt-4">
+          <h3 className="text-black dark:text-white line-clamp-2 scroll-m-20 text-lg sm:text-xl md:text-2xl font-bold tracking-tight">
+            {post?.title}
+          </h3>
+          <p className="font-normal text-gray-800 dark:text-gray-400 text-sm md:text-base mt-2 line-clamp-2">
+            {post?.description}
+          </p>
+        </div>
+      </Link>
+      <div className="mb-2 mt-8 sm:mb-4 flex justify-between items-center">
+        <span className="text-sm text-gray-800 dark:text-gray-400 line-clamp-1">
+          {post?.createdAt}
+        </span>
+        <Bookmark className="cursor-pointer w-4 h-4 sm:w-5 sm:h-5 text-black dark:text-white" />
+      </div>
+      <Separator className="mt-4 sm:mt-8" />
+    </div>
+  );
+};
+
+export default FeaturedPost;
