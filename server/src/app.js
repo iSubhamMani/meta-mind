79c2038fa152bb3d89624e737b41ef699ec871d0
Subham Mani <manisubham09@gmail.com>
2024-05-18 22:28:45 +0530

    Add like route to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -22,9 +22,11 @@ app.use(express.static("public"));
 import errorHandler from "./utils/errorHandler.js";
 import postRouter from "./routes/post.route.js";
 import userRouter from "./routes/user.route.js";
+import likeRouter from "./routes/like.route.js";
 
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/likes", likeRouter);
 
 app.use(errorHandler);
 
