5fb085db80943abfc5e0c2a20c9e72bd16cc4b93
Subham Mani <manisubham09@gmail.com>
2024-05-19 16:51:31 +0530

    Add font-source class to Input component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -4,7 +4,7 @@ const Input = () => {
       type="text"
       placeholder="Search..."
       autoComplete="off"
-      className="w-full text-base text-black dark:text-white bg-transparent focus-within:outline-none px-4"
+      className="font-source w-full text-base text-black dark:text-white bg-transparent focus-within:outline-none px-4"
     />
   );
 };
