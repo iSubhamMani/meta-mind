2ad919d532e2f88fff996674d4420e9d19c35b1a
Subham Mani <manisubham09@gmail.com>
2024-06-02 00:36:10 +0530

    Update SERVER_URL constant in constants.ts

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,6 @@
 const DEFAULT_PROFILE_PHOTO =
   "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg";
 
-const SERVER_URL = "https://meta-mind-wpip.vercel.app/";
+const SERVER_URL = "https://meta-mind-wpip.vercel.app";
 
 export { DEFAULT_PROFILE_PHOTO, SERVER_URL };
